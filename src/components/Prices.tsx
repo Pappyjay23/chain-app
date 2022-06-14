60a8a2817a0d4e293c94a78ff1684668d0cacd3f
Pappyjay23 <pjinadu02@gmail.com>
2022-06-14 22:04:32 +0100

    Correction to the background image in the pricing cards

--- src/components/Prices.tsx
+++ src/components/Prices.tsx
@@ -43,8 +43,8 @@ const Prices = () => {
 						<div className="flex justify-center mb-5">
 							<a href='/' className="text-blue-600 text-sm px-4 py-2 border border-blue-600 rounded-full font-bold">Purchase This Plan Now</a>
 						</div>
-						<div className='absolute bottom-0 right-0 -z-10'>
-							<img src={item.bgdBottom} alt='Price Bgd' />
+						<div className='absolute bottom-0 left-0 right-0 -z-10'>
+							<img src={item.bgdBottom} alt='Price Bgd' className="w-full" />
 						</div>
 					</div>
 				))}
