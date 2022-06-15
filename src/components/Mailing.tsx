319befbc3db3a66337b3024bdc27045570bf943c
Pappyjay23 <pjinadu02@gmail.com>
2022-06-15 18:56:37 +0100

    Adjusted border color on mailing form buttons

--- src/components/Mailing.tsx
+++ src/components/Mailing.tsx
@@ -4,7 +4,7 @@ import Logo from "../images/white-logo.png";
 
 const Mailing = () => {
 	return (
-		<div id="mailing" className='mailing px-5 py-10'>
+		<div id='mailing' className='mailing px-5 py-10'>
 			<h1 className='text-black text-3xl leading-normal capitalize text-center font-extrabold mb-5'>
 				Join our mailing list to recieve the news & latest trends
 			</h1>
@@ -12,11 +12,11 @@ const Mailing = () => {
 				<input
 					type='email'
 					placeholder='Email address...'
-					className='text-slate-800 mb-5 shadow-sm shadow-slate-300 border border-slate-400 placeholder:text-slate-800 outline-none rounded-full px-5 py-3'
+					className='text-slate-800 mb-5 shadow-sm shadow-slate-300 border border-slate-100 placeholder:text-slate-800 outline-none rounded-full px-5 py-3'
 				/>
 				<button
 					type='submit'
-					className='text-blue-600 shadow-sm shadow-slate-300 border border-slate-400 bg-white rounded-full px-5 py-3'>
+					className='text-blue-600 shadow-sm shadow-slate-300 border border-slate-100 bg-white rounded-full px-5 py-3'>
 					Subscribe now
 				</button>
 			</form>
@@ -24,9 +24,9 @@ const Mailing = () => {
 				{FooterLinks.map((item, index) => (
 					<div key={index} className='text-white text-center mb-16'>
 						<p className='font-bold text-xl mb-5'>{item.title}</p>
-						<div className="flex flex-col">
+						<div className='flex flex-col'>
 							{item.links.map((link, index) => (
-								<a href='/' key={index} className='mb-3 text-sm'>
+								<a href='#home' key={index} className='mb-3 text-sm'>
 									{link}
 								</a>
 							))}
@@ -35,9 +35,11 @@ const Mailing = () => {
 				))}
 			</div>
 			<div>
-				<div className='flex justify-center mb-5'>
-					<img src={Logo} alt='logo' />
-				</div>
+				<a href='#home'>
+					<div className='flex justify-center mb-5'>
+						<img src={Logo} alt='logo' />
+					</div>
+				</a>
 				<p className='text-white text-xs text-center'>
 					Copyright © 2022 Chain App Dev Company. All Rights Reserved.
 				</p>
